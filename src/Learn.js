b6c132160c5a36f6f6a3303d21f1013b3362d179
yusra khan <yusraahmedkhan5@gmail.com>
2021-05-26 18:30:41 +0500

    Update Learn.js

--- src/Learn.js
+++ src/Learn.js
@@ -4,7 +4,7 @@ function Learn(props){
     return(
         <div>
             <h2>Learn {props.course}.</h2>
-            <h3>prerequisite: {props.lang}</h3>
+            <h3>Prerequisite: {props.lang}</h3>
 
         </div>
     )
